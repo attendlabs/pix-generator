dd3823021018a404a8faf2dcf458e0619fc0d447
Ramon Silva <silva018-mg@yahoo.com.br>
2021-09-07 14:57:41 -0300

    Adding pix generators

--- src/helpers.ts
+++ src/helpers.ts
@@ -7,7 +7,6 @@ export const getCRC16 = (crc16: string, payload: string): string => {
 	const flag = payload.length;
 
 	if ((flag) > 0) {
-
 		for (let offset = 0; offset < flag; offset++) {
 			result ^= (payload[offset].charCodeAt(0) << 8);
 			for (let bitwise = 0; bitwise < 8; bitwise++) {
@@ -20,10 +19,15 @@ export const getCRC16 = (crc16: string, payload: string): string => {
 };
 
 
-export const merchant_account_info = (merchant_account_info: string, merchant_gui: string, merchant_key: string, merchant_gui_url: string, seller_key: string): string => {
+export const merchant_account_info = (pix_key: string, merchant_account_info: string, merchant_gui: string, merchant_key: string, merchant_gui_url: string): string => {
 
-	const result = `${merchant_gui}${merchant_gui_url.length}${merchant_gui_url}${merchant_key}${seller_key.length}${seller_key}`;
+	const result = `${merchant_gui}${merchant_gui_url.length}${merchant_gui_url}${merchant_key}${pix_key.length}${pix_key}`;
 	return `${merchant_account_info}${result.length}${result}`;
+};
+
+export const format_info = (id: string, value: string): string => {
+	const size = get_pad_length(value, 2);
+	return `${id}${size}${value}`;
 };
 
 export const get_pad_length = (input: string, size: number): string => {
@@ -31,7 +35,7 @@ export const get_pad_length = (input: string, size: number): string => {
 	return result.padStart(size, '0');
 };
 
-export const additional_field = (id_additional_field: string, id_tx_id: string, value: string): string => {
+export const get_additional_field = (id_additional_field: string, id_tx_id: string, value: string): string => {
 	const tx_id_size = get_pad_length(value, 2);
 	const additional_size = get_pad_length(`${id_tx_id}${tx_id_size}${value}`, 2);
 
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,38 @@
 import QRCode from 'qrcode';
+import { merchant_account_info, format_info, get_additional_field, getCRC16 } from './helpers';
+import {
+	MERCHANT_ACCOUNT, MERCHANT_ACCOUNT_GUI, MERCHANT_ACCOUNT_KEY, MERCHANT_GUI_URL, PAYLOAD_FORMAT_INDICATOR, COUNTRY_CODE, CRC16,
+	MERCHANT_CATEGORY_CODE, TRANSACTION_AMOUNT, MERCHANT_NAME, MERCHANT_CITY, ADDITIONAL_DATA_FIELD_TEMPLATE, ADDITIONAL_DATA_FIELD_TEMPLATE_TXID
+} from './constants';
 
-export const generateQR = async (text: string): Promise<string> => {
-	try {
-		return await QRCode.toDataURL(text);
-	} catch (err) {
-		throw new Error('Erro ao gerar qrcode');
+export default class PIX {
+
+	static code = (tx_id: string, pix_key: string, amount: string, seller_name: string, seller_city: string): string | Error => {
+
+		if (!tx_id || !pix_key || !amount || !seller_name) {
+			throw new Error('Parameters required');
+		}
+
+		const merchant_account = merchant_account_info(pix_key, MERCHANT_ACCOUNT, MERCHANT_ACCOUNT_GUI, MERCHANT_ACCOUNT_KEY, MERCHANT_GUI_URL);
+		const payload_indicator = format_info(PAYLOAD_FORMAT_INDICATOR, '01');
+		const country = format_info(COUNTRY_CODE, 'BR');
+		const merchant_category = format_info(MERCHANT_CATEGORY_CODE, '0000');
+		const translated_currency = format_info(TRANSACTION_AMOUNT, amount);
+		const merchant_name = format_info(MERCHANT_NAME, seller_name);
+		const merchant_city = format_info(MERCHANT_CITY, seller_city);
+		const additional_field = get_additional_field(ADDITIONAL_DATA_FIELD_TEMPLATE, ADDITIONAL_DATA_FIELD_TEMPLATE_TXID, tx_id);
+
+		const payload = `${payload_indicator}${merchant_account}${merchant_category}${translated_currency}${country}${merchant_name}${merchant_city}${additional_field}`;
+
+		return `${payload}${getCRC16(CRC16, payload)}`;
 	}
-};
 
-( async() => {
-	console.log(await generateQR('teste'));
-})();
+	static qrcode = async (code: string): Promise<string|Error> => {
+		try {
+			return await QRCode.toDataURL(code);
+		} catch (err) {
+			throw new Error('Erro ao gerar qrcode');
+		}
+	}
+
+}
